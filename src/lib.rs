ff0504a0403e07884434483bfb29d3a6a8c69e55
Param Pal Singh <singhparampal97@gmail.com>
2023-09-26 21:32:26 +0200

    added lib + health_check endpoint + initial tests

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::net::TcpListener;
+
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+async fn healtch_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(healtch_check))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net;
+
+use zero2prod::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = net::TcpListener::bind("localhost:8000")?;
+    run(listener)?.await
 }
