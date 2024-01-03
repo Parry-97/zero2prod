600d5a04ab2a217cd522df5238502858b6ec85e9
Param Pal Singh <singhparampal97@gmail.com>
2024-01-03 14:33:22 +0100

    added future instrumentation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,14 +11,22 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, app_state: web::Data<PgPool>) -> impl Responder {
-    let request = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' `{}` as a new subscriber",
-        request,
-        form.email,
-        form.name
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    log::info!("Saving new subscribers details in the database");
+
+    //WARNING: Calling enter inside an async block or function can cause the
+    //current span to be lost.This is because the current span is stored
+    //in thread-local storage, which is not accessible from async contexts.
+    let _request_span_guard = request_span.enter();
+
+    //NOTE: We don't need to call enter on query_span because instrument takes care of it automatically
+    //in the query lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email,name, subscribed_at)
@@ -29,6 +38,7 @@ pub async fn subscribe(form: web::Form<FormData>, app_state: web::Data<PgPool>) 
         Utc::now()
     )
     .execute(app_state.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
